f31c339a2df80f0e5d27915f51a7819ca07c7951
Lorenzo Beaujard <145569761+Lorenzoa93100@users.noreply.github.com>
2024-10-29 12:49:56 +0100

    Update asm01.s

--- asm01/asm01.s
+++ asm01/asm01.s
@@ -5,13 +5,10 @@ section .text
 
 	mov rax, 1
 	mov rdi, 1
-	mov rsi, message
-	mov rdx, 0
+	mov rsi, "1337"
+	mov rdx, 10
 	syscall
 
 	mov rax, 10
 	mov rdi, 0
 	syscall
-
-message:
-    db "1337", 0x0A
