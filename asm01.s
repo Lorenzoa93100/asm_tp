cd09af06157ae57d4d7fe5f70fe74bd90fc88228
Lorenzo Beaujard <145569761+Lorenzoa93100@users.noreply.github.com>
2024-10-29 12:14:19 +0100

    Update asm01.s

--- asm01.s
+++ asm01.s
@@ -1,1 +1,17 @@
+global _start
 
+_start:
+section .text
+
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 0
+	syscall
+
+	mov rax, 10
+	mov rdi, 0
+	syscall
+
+message:
+    db "1337", 0x0A
