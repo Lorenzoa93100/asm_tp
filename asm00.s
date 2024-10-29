5350522d5851bb7e63c0b3092f4b62fe8dedbaf6
Lorenzo Beaujard <145569761+Lorenzoa93100@users.noreply.github.com>
2024-10-29 12:14:44 +0100

    Update asm00.s

--- asm00.s
+++ asm00.s
@@ -2,16 +2,6 @@ global _start
 
 _start:
 section .text
-
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, message
-	mov rdx, 0
-	syscall
-
 	mov rax, 60
 	mov rdi, 0
 	syscall
-
-message:
-    db "Hello, World!", 0x0A
