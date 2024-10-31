8ef182b1322ed5b59745f273ce735261f3938d57
Lorenzo Beaujard <145569761+Lorenzoa93100@users.noreply.github.com>
2024-10-31 10:19:30 +0100

    Update asm03.s

--- asm03/asm03.s
+++ asm03/asm03.s
@@ -1,33 +1,35 @@
 global _start
-
-section .bss
-    buff resb 4
-
 section .data
-    msg db "1337", 4
+    msg db "1337", 10
 
 section .text
+_start:
     pop rax
     cmp rax, 2
     jne _exit_error
 
-    pop rax
+    pop rax 
     pop rax
 
     mov bl, [rax]
-    mov bl, 0x034
+    cmp bl, 0x34
     jne _exit_error
 
     mov bl, [rax + 1]
-    mov bl, 0x032
+    cmp bl, 0x32
     jne _exit_error
 
     mov bl, [rax + 2]
-    mov bl, 0x00
+    cmp bl, 0x00
     jne _exit_error
-
+    
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, 10
+    syscall
 _exit:
-    ;sys_exit_error
+    ;exit
     mov rax, 60
     mov rdi, 0
     syscall
