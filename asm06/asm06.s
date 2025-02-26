fcf9b95aae0b81783a58308aef41f3297835322a
Lorenzo Beaujard <lbeaujard3@myges.fr>
2025-02-26 12:37:43 +0100

    asm06 nombre negatifs

--- asm06/asm06.s
+++ asm06/asm06.s
@@ -29,10 +29,19 @@ _start:
     
     jmp _exit          ; Quitter proprement
 
-; Convertir une chaîne en entier (atoi)
+; Convertir une chaîne en entier (atoi) avec support des négatifs
 atoi:
     xor rax, rax       ; Réinitialise rax (résultat)
     xor rcx, rcx       ; Initialise compteur
+    mov r9, 0          ; Flag pour nombre négatif (0 = positif)
+    
+    ; Vérifier si commence par un signe '-'
+    movzx rdx, byte [rdi]
+    cmp rdx, '-'
+    jne atoi_loop      ; Si pas de signe, commencer la conversion
+    mov r9, 1          ; Activer le flag négatif
+    inc rdi            ; Avancer au premier chiffre après le signe
+    
 atoi_loop:
     movzx rdx, byte [rdi + rcx]  ; Charge un caractère
     test rdx, rdx       ; Vérifie si fin de chaîne (NULL)
@@ -44,14 +53,29 @@ atoi_loop:
     add rax, rdx        ; Ajoute le chiffre converti
     inc rcx             ; Avance au prochain caractère
     jmp atoi_loop       ; Continue
+    
 atoi_end:
+    test r9, r9         ; Vérifier si nombre négatif
+    jz atoi_positive    ; Si positif, ne rien faire
+    neg rax             ; Sinon, négation du résultat
+atoi_positive:
     ret
 
-; Convertir un entier en chaîne ASCII (itoa)
+; Convertir un entier en chaîne ASCII (itoa) avec support des négatifs
 itoa:
     mov r9, rdi        ; Sauvegarde l'adresse du buffer
+    mov r10, 0         ; Flag pour nombre négatif (0 = positif)
+    
+    ; Vérifier si le nombre est négatif
+    test rax, rax
+    jns itoa_positive  ; Si non négatif (sign flag=0), continuer
+    mov r10, 1         ; Activer le flag négatif
+    neg rax            ; Rendre le nombre positif pour la conversion
+    
+itoa_positive:
     mov rcx, 0         ; Compteur de chiffres
     mov rbx, 10        ; Base 10 pour division
+    
 itoa_loop:
     xor rdx, rdx       ; Vide rdx pour div propre
     div rbx            ; rax = quotient, rdx = reste
@@ -63,6 +87,13 @@ itoa_loop:
     
     mov r8, rcx        ; Sauvegarde le nombre de chiffres
     
+    ; Si nombre négatif, ajouter le signe '-'
+    test r10, r10      ; Vérifier le flag négatif
+    jz itoa_pop_loop   ; Si positif, passer directement à l'affichage
+    mov byte [rdi], '-'; Sinon, ajouter un signe moins
+    inc rdi            ; Avancer le pointeur
+    inc r8             ; Augmenter la taille du résultat
+    
 itoa_pop_loop:
     pop rax            ; Récupère un chiffre
     mov [rdi], al      ; Stocke dans le buffer
@@ -71,7 +102,7 @@ itoa_pop_loop:
     jnz itoa_pop_loop  ; Répète jusqu'à vider la pile
     
     mov byte [rdi], 0x0A  ; Ajoute '\n'
-    inc r8            ; Compter '\n' dans la longueur totale
+    inc r8             ; Compter '\n' dans la longueur totale
     ret
 
 ; Quitter proprement
