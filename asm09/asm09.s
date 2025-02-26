e7a9fb97c9b1215da8d8ceabdb110ec41908b0b0
Lorenzo Beaujard <lbeaujard3@myges.fr>
2025-02-26 15:09:54 +0100

    commit asm09

--- asm09/asm09.s
+++ asm09/asm09.s
@@ -2,49 +2,81 @@ global _start
 
 section .data
     hex_chars db "0123456789ABCDEF", 0   ; Table des caractères hexadécimaux
-    buffer db 16 dup(0)                  ; Tampon pour stocker le résultat
+    newline db 0x0A                      ; Caractère nouvelle ligne
 
 section .bss
+    buffer resb 64                       ; Tampon pour stocker le résultat (plus grand pour binaire)
     num resq 1                           ; Espace pour stocker le nombre
 
 section .text
 
 _start:
-    ; Vérification du nombre d'arguments (argc == 2)
+    ; Vérification du nombre d'arguments (argc == 2 ou argc == 3)
     pop rax                              ; argc
     cmp al, 2
+    je decimal_to_hex                    ; Si 2 arguments, conversion décimal vers hexadécimal
+    cmp al, 3                            ; Si 3 arguments, vérifier si c'est -b
     jne _exit_error
 
+    ; Vérifier l'option -b
+    pop rax                              ; Ignorer argv[0]
+    pop rdi                              ; argv[1] (option)
+    cmp byte [rdi], '-'
+    jne _exit_error
+    cmp byte [rdi+1], 'b'
+    jne _exit_error
+    cmp byte [rdi+2], 0
+    jne _exit_error
+
+    ; Lire le nombre depuis argv[2]
+    pop rsi                              ; argv[2] (chaîne)
+    call atoi                            ; Convertir la chaîne en entier (dans rax)
+    
+    ; Conversion en binaire
+    mov rbx, 2                           ; Base binaire
+    jmp convert
+
+decimal_to_hex:
     ; Lecture de l'argument (argv[1])
-    pop rax                              ; argv
+    pop rax                              ; Ignorer argv[0]
     pop rsi                              ; argv[1] (chaîne)
     call atoi                            ; Convertir la chaîne en entier (dans rax)
-
+    
     ; Conversion en hexadécimal
+    mov rbx, 16                          ; Base hexadécimale
+
+convert:
     mov rcx, 0                           ; Compteur pour buffer
 convert_loop:
     xor rdx, rdx                         ; RDX = 0 (remainder)
-    mov rbx, 16                          ; Base hexadécimale
-    div rbx                              ; RAX /= 16, reste dans RDX
-    mov bl, byte [hex_chars + rdx]       ; Charger le caractère correspondant
-    mov byte [buffer + rcx], bl          ; Stocker dans le tampon
+    div rbx                              ; RAX /= base, reste dans RDX
+    mov r8b, byte [hex_chars + rdx]      ; Charger le caractère correspondant
+    mov byte [buffer + rcx], r8b         ; Stocker dans le tampon
     inc rcx                              ; Incrémenter l'index
-    cmp rcx, 16                          ; Vérifier si le tampon est plein
+    cmp rcx, 64                          ; Vérifier si le tampon est plein
     je _exit_error                       ; Si oui, erreur (valeur trop grande)
     test rax, rax                        ; Si RAX == 0, fin de conversion
     jnz convert_loop
 
     ; Afficher le résultat (inversé)
-    lea rsi, [buffer + rcx]              ; Pointeur à la fin du résultat
+    lea rsi, [buffer + rcx - 1]          ; Pointeur à la fin du résultat
+    mov r9, rcx                          ; Sauvegarder le compteur
 print_loop:
-    dec rsi                              ; Reculer d'un caractère
     mov al, byte [rsi]                   ; Charger le caractère
-    mov edi, 1                           ; FD stdout
+    mov rdi, 1                           ; FD stdout
     mov rax, 1                           ; write syscall
     mov rdx, 1                           ; Longueur 1
     syscall
-    cmp rsi, buffer                      ; Fin d'affichage ?
-    ja print_loop                        ; Continuer tant que rsi > buffer
+    dec rsi                              ; Reculer d'un caractère
+    dec r9                               ; Décrémenter le compteur
+    jnz print_loop                       ; Continuer tant que r9 > 0
+
+    ; Ajouter un saut de ligne
+    mov rax, 1                           ; write syscall
+    mov rdi, 1                           ; FD stdout
+    mov rsi, newline                     ; Nouvelle ligne
+    mov rdx, 1                           ; Longueur 1
+    syscall
 
     ; Sortie propre
     jmp _exit
@@ -62,12 +94,13 @@ _exit_error:
 ; Fonction atoi : Convertit une chaîne en entier
 atoi:
     xor rax, rax                         ; Initialiser le résultat à 0
-    xor rcx, rcx                         ; Effacer RCX
 atoi_loop:
     movzx rcx, byte [rsi]                ; Charger un caractère
     test rcx, rcx                        ; Fin de chaîne ?
     je atoi_done
     sub rcx, '0'                         ; Convertir en chiffre (0-9)
+    cmp rcx, 9                           ; Vérifier si c'est un chiffre valide
+    ja _exit_error                       ; Si non, sortir avec erreur
     imul rax, rax, 10                    ; Multiplier le résultat par 10
     add rax, rcx                         ; Ajouter le chiffre au résultat
     inc rsi                              ; Passer au caractère suivant
